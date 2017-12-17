446b612b0a2c4834b7301c7c61d9ef515e27e71b
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-12-17 19:19:23 +0200

    Add start command

--- src/flaskbox/reader.py
+++ src/flaskbox/reader.py
@@ -1,6 +1,14 @@
 """Reader stuff"""
+import yaml
 
 
 class ApplicationReader:
-    def __init__(self, application):
-        self.app_yml = application
+    def __init__(self):
+        self.app_yml = 'flaskbox.yml'
+        self.blueprints = []
+
+    def _read(self):
+        with open(self.app_yml, 'r') as config:
+            yml = yaml.load(config)
+            name = yml['application']['name']
+        return name
