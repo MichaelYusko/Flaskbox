de733cd1eb98b546f199d3b19aa87bc078097dd6
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-12-20 01:03:19 +0200

    Add tests

---
+++ tests/unit/test_helpers.py
@@ -0,0 +1,8 @@
+from src.flaskbox.helpers import create_init_file
+
+
+def test_init_file(tmpdir):
+    """Test init file"""
+    file = tmpdir.join('flaskbox.yml')
+    create_init_file()
+    assert file
