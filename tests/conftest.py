386795635c9c2550cfa257c5e8e2dc16cd4afb36
MichaelYusko <mykhaylo.yusko@gmail.com>
2018-02-03 13:38:02 +0200

    Add a few tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
-import yaml
+import pytest
+
+from flaskbox.helpers import create_init_file
+from flaskbox.reader import YAMLBaseReader
 
 
-def create_init_file():
+@pytest.fixture(autouse=True)
+def create_yaml_file():
     """Create Init file"""
-    base_app = {'application': 'My restful API'}
-    with open('flaskbox.yml', 'w') as file:
-        yaml.dump(base_app, file, default_flow_style=False)
+    create_init_file()
+
+
+@pytest.fixture
+def base_app():
+    return YAMLBaseReader().read()
---
+++ tests/unit/test_config.py
@@ -0,0 +1,31 @@
+from flaskbox.config import config
+
+
+def test_get_name_helper(base_app):
+    """Get an application name"""
+    name = config.get_name(base_app)
+    assert name == base_app[0]['application']['name']
+
+
+def test_get_routes(base_app):
+    """Test if an array with routes exists
+    """
+    routes = config.get_routes(base_app)
+    assert (isinstance(routes, list), len(routes)) == (True, 1)
+
+
+def test_get_fields(base_app):
+    """If fields exists, return an array with field objects
+    """
+    fields = None
+    routes = config.get_routes(base_app)
+    for route in routes:
+        fields = config.get_fields(route)
+    assert (isinstance(fields, list), len(fields)) == (True, 4)
+
+
+def test_if_port_not_exists(base_app):
+    """If port not exists, return default 5000 port
+    """
+    port = config.get_port(base_app)
+    assert 5000 == port
